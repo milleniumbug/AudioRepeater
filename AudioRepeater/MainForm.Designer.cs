6bd54ea1a1641346ceea7cd3a77558dbecb4301a
milleniumbug <milleniumbug@protonmail.com>
2024-06-03 21:02:21 +0200

    First commit

---
+++ AudioRepeater/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AudioRepeater
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RecordingStatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RecordingStatusLabel
+            // 
+            this.RecordingStatusLabel.AutoSize = true;
+            this.RecordingStatusLabel.Location = new System.Drawing.Point(31, 26);
+            this.RecordingStatusLabel.Name = "RecordingStatusLabel";
+            this.RecordingStatusLabel.Size = new System.Drawing.Size(56, 13);
+            this.RecordingStatusLabel.TabIndex = 0;
+            this.RecordingStatusLabel.Text = "Recording";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RecordingStatusLabel);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RecordingStatusLabel;
+    }
+}
+
---
+++ AudioRepeater/MainForm.cs
@@ -0,0 +1,85 @@
+﻿using NAudio.Utils;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AudioRepeater
+{
+    public partial class MainForm : Form
+    {
+        BufferedWaveProvider buffer;
+        WaveOutEvent output;
+        WasapiLoopbackCapture input;
+        RawSourceWaveStream stream;
+        Stopwatch stopwatch = new Stopwatch();
+        int totalSeconds = 5;
+        int restartSeconds = 10;
+        bool recording = true;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            GlobalHotKey.RegisterHotKey("Alt + Shift + S", () => Play());
+            input = new WasapiLoopbackCapture();
+            output = new WaveOutEvent();
+            
+            buffer = new BufferedWaveProvider(input.WaveFormat);
+            buffer.BufferDuration = TimeSpan.FromSeconds(totalSeconds);
+            buffer.ReadFully = true;
+            input.DataAvailable += RecordData;
+            input.StartRecording();
+        }
+
+        private void RestartRecording()
+        {
+            RecordingStatusLabel.Text += "\nRecording";
+            output.Stop();
+            recording = true;
+            input.StartRecording();
+        }
+
+        private void RecordData(object sender, WaveInEventArgs e)
+        {
+            buffer.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        public async void Play()
+        {
+            RecordingStatusLabel.Text = "Playing";
+            if (recording)
+            {
+                input.StopRecording();
+                output.Stop();
+                var bytes = new byte[buffer.BufferedBytes];
+                buffer.Read(bytes, 0, bytes.Length);
+                stream = new RawSourceWaveStream(bytes, 0, bytes.Length, input.WaveFormat);
+                output.Init(stream);
+                recording = false;
+            }
+
+            output.Stop();
+            var s = stream;
+            if(s != null)
+            {
+                s.Position = 0;
+            }
+
+            stream.Position = 0;
+            output.Play();
+            stopwatch.Restart();
+            await Task.Delay(TimeSpan.FromSeconds(restartSeconds));
+            if(!recording && stopwatch.Elapsed >= TimeSpan.FromSeconds(restartSeconds))
+            {
+                RestartRecording();
+            }
+        }
+    }
+}
